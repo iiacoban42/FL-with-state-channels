7f455c4d72f7547bc5f0126369d7e3d86dd13ca3
iiacoban42 <i.p.iacoban@gmail.com>
2024-12-12 20:50:50 +0100

    Update startup script with pwd path

--- start.sh
+++ start.sh
@@ -8,7 +8,8 @@ num_iterations=$((number_of_clients+1))
 # Loop through the range of iterations
 for ((i=0; i<num_iterations; i++)); do
     # Format the command with the current index
-    base_command="cd Desktop/fl-app-channel && ./app-channel demo --config config/peer_%d.yaml --log-level trace --log-file logs/peer_%d.log"
+    path=$(pwd)
+    base_command="cd "$path" && ./app-channel demo --config config/peer_%d.yaml --log-level trace --log-file logs/peer_%d.log"
     current_command=$(printf "$base_command" "$i" "$i")
 
     # Open a new tab in the terminal and execute the command
